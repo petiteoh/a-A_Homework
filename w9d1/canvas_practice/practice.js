e086b32c33caf7d50128a37e9dc446dfc2af6074
petiteoh <nikkiteoh@gmail.com>
2020-07-05 15:19:51 -0400

    w9d1 homework

--- w9d1/canvas_practice/practice.js
+++ w9d1/canvas_practice/practice.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const canvas = document.getElementById('mycanvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
 
+    ctx.fillStyle = "red";
+    ctx.fillRect(20,10,200,100);
+
+    ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.strokeStyle = 'yellow';
+    ctx.lineWidth = 10;
+    ctx.stroke();
+    ctx.fillStyle = "purple";
+    ctx.fill();
 });
