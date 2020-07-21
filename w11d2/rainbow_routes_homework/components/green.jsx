a5a5965bca33236b3ecf27e760b93227627e6d58
petiteoh <nikkiteoh@gmail.com>
2020-07-20 23:15:57 -0400

    rainbow homework

---
+++ w11d2/rainbow_routes_homework/components/green.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Green = () => (
+  <div>
+    <h2 className="green"></h2>
+  </div>
+);
+
+export default Green;
---
+++ w11d2/rainbow_routes_homework/components/orange.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Orange = () => (
+  <div>
+    <h3 className="orange"></h3>
+    <NavLink exact to="/orange">Orange only</NavLink>
+  </div>
+);
+
+export default Orange;
