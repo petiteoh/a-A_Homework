4b59ea0b985e243408db2f462f98ab0cf6a4abe7
petiteoh <nikkiteoh@gmail.com>
2020-07-22 14:10:30 -0400

    w11d3 projects

---
+++ w11d3/front_end_auth_project/frontend/utils/route_utils.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect, Route, withRouter } from 'react-router-dom';
+
+const mapStatetoProps = state => {
+    return {
+        loggedIn: Boolean(state.session.currentUser)
+    };
+};
+
+// <AuthRoute path="" component={}>
+
+const Auth = ({ loggedIn, path, component: Component }) => {
+    return (
+        <Route
+            path={path}
+            render={props => (
+                loggedIn ? <Redirect to='/' /> : <Component {...props} /> 
+            )}
+        />
+    );
+};
+
+const Protected = ({ loggedIn, path, component: Component }) => {
+    return (
+        <Route 
+            path={path}
+            render={props => (
+                loggedIn ? <Component {...props} /> : <Redirect to='/signup' />
+            )}
+        />
+    );
+};
+
+export const AuthRoute = withRouter(connect(mapStatetoProps)(Auth));
+export const ProtectedRoute = withRouter(connect(mapStatetoProps)(Protected));
