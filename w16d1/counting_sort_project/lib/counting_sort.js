2e2b8dbb99e811fd66f5421937ad15bafdb41853
petiteoh <nikkiteoh@gmail.com>
2020-08-24 12:14:24 -0400

    countingsort project

--- w16d1/counting_sort_project/lib/counting_sort.js
+++ w16d1/counting_sort_project/lib/counting_sort.js
@@ -1,8 +1,33 @@
 function countingSort(arr, max) {
+    const result = [];
+    const counters = new Array(max + 1).fill(0);
 
+    for (let i = 0; i < arr.length; i++) {
+        counters[arr[i]]++
+    }
+
+    for (let i = 0; i < counters.length; i++) {
+        while (counters[i] > 0) {
+            result.push(i);
+            counters[i]--;
+        }
+    }
+
+    return result;
 }
 
 
 module.exports = {
     countingSort
 };
+
+// countingSort
+// k the largest integer 
+// set each element to 0
+// fill each of the counters with the arr[i] element and add only count to it
+// later go through the counters and push the element until that specific counter is 0
+
+// time O(n * k)
+
+// space O(k)
+
