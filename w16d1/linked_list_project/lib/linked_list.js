52fe2e924646f600b37fca2b99cf694b4f20b1bc
petiteoh <nikkiteoh@gmail.com>
2020-09-09 23:33:46 -0700

    removeTail

--- w16d1/linked_list_project/lib/linked_list.js
+++ w16d1/linked_list_project/lib/linked_list.js
@@ -23,25 +23,57 @@
 // TODO: Implement a Linked List Node class here
 class Node {
     constructor(val) {
-
+        this.value = val;
+        this.next = null;
     }
-
 }
 
 // TODO: Implement a Singly Linked List class here
 class LinkedList {
     constructor() {
-
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
 
     // TODO: Implement the addToTail method here
     addToTail(val) {
+        const newNode = new Node(val);
 
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+        }
+
+        this.tail = newNode;
+        this.length++
+        return this;
     }
 
     // TODO: Implement the removeTail method here
     removeTail() {
+        if (!this.head) {
+            return undefined
+        }
 
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+
+        return current;
     }
 
     // TODO: Implement the addToHead method here
