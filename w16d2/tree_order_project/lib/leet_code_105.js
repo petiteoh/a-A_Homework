8bba2632317d0d3a4c6c9504065096b9ff54a701
petiteoh <nikkiteoh@gmail.com>
2020-09-21 13:52:52 -0700

    lc 105

--- w16d2/tree_order_project/lib/leet_code_105.js
+++ w16d2/tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,28 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
+    if (preorder.length === 0 && inorder.length === 0) return null;
 
+    let rootVal = preorder[0];
+    let root = new TreeNode(rootVal, null, null);
+
+    let midIdx = inorder.indexOf(rootVal);
+    let leftInorder = inorder.slice(0, midIdx);
+    let rightInorder = inorder.slice(midIdx + 1);
+
+    let leftPreorder = preorder.filter((val) => leftInorder.includes(val));
+    let rightPreorder = preorder.filter((val) => rightInorder.includes(val));
+
+    let leftTree = buildTree(leftPreorder, leftInorder);
+    let rightTree = buildTree(rightPreorder, rightInorder);
+
+    root.left = leftTree;
+    root.right = rightTree;
+
+    return root;
 }
+
+let preorder = [3, 9, 20, 15, 7];
+let inorder = [9, 3, 15, 20, 7];
+
+console.log(buildTree(preorder, inorder));
