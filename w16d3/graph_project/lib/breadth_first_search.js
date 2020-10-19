e878e6004ebfc533c0eee9b417e05240e2e6996e
petiteoh <nikkiteoh@gmail.com>
2020-10-18 20:58:42 -0700

    bfs

--- w16d3/graph_project/lib/breadth_first_search.js
+++ w16d3/graph_project/lib/breadth_first_search.js
@@ -1,5 +1,13 @@
 function breadthFirstSearch(startingNode, targetVal) {
+    if (!startingNode) return null;
 
+    let node = startingNode;
+    let queue = []
+    if (node.val === targetVal) {
+        return node;
+    } else {
+        queue.push(node.neighbor)
+    }
 }
 
 module.exports = {
