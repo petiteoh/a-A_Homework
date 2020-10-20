ce965e4cba4147c40dd4b9b84fcf0f5bdd867f92
petiteoh <nikkiteoh@gmail.com>
2020-10-20 14:58:45 -0700

    isNewRegion

--- w16d3/graph_project/lib/num_regions.js
+++ w16d3/graph_project/lib/num_regions.js
@@ -2,6 +2,16 @@ function numRegions(graph) {
 
 }
 
+function isNewRegion(node, graph, visited) {
+    if (visited.has(node)) return false;
+    visited.add(node);
+    graph[node].forEach((neighbor) => {
+        isNewRegion(neighbor, graph, visited);
+    });
+
+    return true;
+}
+
 module.exports = {
     numRegions
 };
