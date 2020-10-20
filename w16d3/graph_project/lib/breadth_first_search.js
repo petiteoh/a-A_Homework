3bffa1314ead7ddd3b5597e7f78a791b42f25ef6
petiteoh <nikkiteoh@gmail.com>
2020-10-19 18:55:34 -0700

    name bug

--- w16d3/graph_project/lib/breadth_first_search.js
+++ w16d3/graph_project/lib/breadth_first_search.js
@@ -1,12 +1,12 @@
 function breadthFirstSearch(startingNode, targetVal) {
-    let queue = [startingNode]
+    let queue = [ startingNode ];
     let visited = new Set();
 
     while (queue.length) {
         let node = queue.shift();
         if (visited.has(node)) continue;
         visited.add(node);
-        if (node.val === targetValue) return node;
+        if (node.val === targetVal) return node;
 
         queue.push(...node.neighbors);
     }
