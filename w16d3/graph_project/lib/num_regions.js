dbfc37cd46cc38d98d2a6f2ec408cf62aa5515fe
petiteoh <nikkiteoh@gmail.com>
2020-10-20 15:01:55 -0700

    numRegrions

--- w16d3/graph_project/lib/num_regions.js
+++ w16d3/graph_project/lib/num_regions.js
@@ -1,5 +1,12 @@
 function numRegions(graph) {
+    let visited = new Set();
+    let regions = 0;
 
+    for (let node in graph) {
+        if (isNewRegion(node, graph, visited)) regions++;
+    };
+
+    return regions;
 }
 
 function isNewRegion(node, graph, visited) {
