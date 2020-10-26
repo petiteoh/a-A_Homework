af19534d5ac01ad1798798884674fdbf3341464f
petiteoh <nikkiteoh@gmail.com>
2020-10-25 18:07:56 -0700

    ismaxheap function

--- w16d3/heaps_project/lib/is_heap.js
+++ w16d3/heaps_project/lib/is_heap.js
@@ -1,6 +1,7 @@
 // you may assume that the array will always have a null element at the 0-th index
 function isMaxHeap(array, idx=1) {
-
+        let isComplete = array.every((el) => el !== undefined);
+        return isComplete && _isMaxHeap(array);
 }
 
 function _isMaxHeap(array) {
