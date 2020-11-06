215b630d89f177f9b479f1d3f6b466e2c37b5f23
petiteoh <nikkiteoh@gmail.com>
2020-11-06 12:15:37 -0800

    trie costrutor

--- w16d3/trie_project/lib/trie.js
+++ w16d3/trie_project/lib/trie.js
@@ -2,14 +2,34 @@ class Node {
     constructor() {
         this.children = {};
         this.isTerminal = false;
-    }
-}
+    };
+};
 
 class Trie {
     constructor() {
+        this.root = new Node();
+    };
 
-    }
-}
+    insertRecur(word) {
+
+    };
+
+    insertIter(word) {
+
+    };
+
+    searchRecur(word) {
+
+    };
+
+    searchIter(word) {
+
+    };
+
+    wordsWithPrefix(prefix) {
+
+    };
+};
 
 module.exports = {
     Node,
