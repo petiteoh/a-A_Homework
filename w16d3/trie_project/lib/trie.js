2b47ed7e5eb2eae7b808381d47fd5e69350e8c19
petiteoh <nikkiteoh@gmail.com>
2020-11-07 17:50:15 -0800

    searchIter

--- w16d3/trie_project/lib/trie.js
+++ w16d3/trie_project/lib/trie.js
@@ -57,7 +57,19 @@ class Trie {
     };
 
     searchIter(word) {
+        let node = this.root;
+        
+        for (let i = 0; i < word.length; i++) {
+            let letter = word[i];
 
+            if (!(letter in node.children)) {
+                return false;
+            };
+
+            node = node.children[letter];
+        };
+
+        return node.isTerminal;
     };
 
     wordsWithPrefix(prefix) {
