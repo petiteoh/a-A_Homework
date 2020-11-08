a129ea4e9735e91b7cda2a249a6a5f78e7e0f293
petiteoh <nikkiteoh@gmail.com>
2020-11-07 17:48:50 -0800

    insertRecur

--- w16d3/trie_project/lib/trie.js
+++ w16d3/trie_project/lib/trie.js
@@ -11,7 +11,16 @@ class Trie {
     };
 
     insertRecur(word) {
+        let letter = word[0];
+        if (!(letter in root.children)) {
+            root.children[letter] = new Node();
+        };
 
+        if (word.length === 1) {
+            root.children[letter].isTerminal = true;
+        } else {
+            this.insertRecur(word.slice(1), root.children[letter]);
+        };
     };
 
     insertIter(word) {
